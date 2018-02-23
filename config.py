bdf9dcf184b361d0280660d0f398b64860ff84bb
tigeraus <tigeraus@icloud.com>
2018-02-23 19:24:59 +0800

    remove config

--- config.py
+++
@@ -1,4 +0,0 @@
-#初始化apikey，secretkey,url
-apikey = 'a16940b7-fd18-44eb-af1b-7ac80ad04b56'
-secretkey = '25861EEE3751B7212CF6F80C64E927C9'
-okcoinRESTURL = 'www.okex.com'   #请求注意：国内账号需要 修改为 www.okcoin.cn  
